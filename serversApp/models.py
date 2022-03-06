820ed2a5c63f0c30c42e99cf6b802c4fb45c2e57
zhongxiaoting <z1915270314@163.com>
2022-03-06 14:15:53 +0800

    first commit

---
+++ serversApp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+
+class Server(models.Model):
+    """
+    服务器SN、工号等信息
+    """
+    sn = models.CharField(max_length=30, verbose_name="服务器SN")
+    worker = models.CharField(max_length=10, verbose_name="工号")
+    ip = models.CharField(max_length=30, verbose_name="IP号")
+    submission_date = models.DateTimeField(default=timezone.now, verbose_name="提交时间")
+
+    class Meta:
+        verbose_name = "服务器信息"
+        verbose_name_plural = verbose_name
+        ordering = ['-submission_date']
+        db_table = "Server"
+
+    def __str__(self):
+        return self.sn
